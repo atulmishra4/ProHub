73a160d0f7a98d74dcb1ab20b9b6961e8a3101cd
atulmishra4 <atulllmishra4@gmail.com>
2025-10-08 21:33:27 +0530

    fix: remove unused import from page component

--- my-app/src/components/header/page.jsx
+++ my-app/src/components/header/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 import React, {useState} from 'react'
 import { motion } from 'framer-motion';
-import { href } from 'react-router-dom';
+// import { href } from 'react-router-dom';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
